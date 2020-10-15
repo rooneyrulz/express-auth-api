b933d05e8f3ea8c86b82226faa4cc5dedcac4142
ruzny <ruzny@ruzny.com>
2020-10-15 21:36:25 +0530

    added express-validator middlewares

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,66 +1,69 @@
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
+const { validationResult } = require('express-validator');
 
 const User = require('../models/User');
 
-const signUp = async(req, res, next) => {
-    const { email, password } = req.body;
-    if (!email.trim()) return res.status(400).send('Email is required!');
-    if (!password.trim()) return res.status(400).send('Password is required!');
-    try {
-        const isUserExist = await User.findOne({ email }).lean();
-        if (isUserExist)
-            return res.status(409).send('Email has already been taken!');
+const signUp = async (req, res, next) => {
+  const errors = validationResult(req);
+  const { email, password } = req.body;
+  if (!errors.isEmpty())
+    return res.status(400).json({ errors: errors.array() });
+  try {
+    const isUserExist = await User.findOne({ email }).lean();
+    if (isUserExist)
+      return res.status(409).send('Email has already been taken!');
 
-        const hashedPWD = await bcrypt.hash(password, 12);
-        const user = await new User({ email, password: hashedPWD }).save();
+    const hashedPWD = await bcrypt.hash(password, 12);
+    const user = await new User({ email, password: hashedPWD }).save();
 
-        const token = jwt.sign({ id: user._id }, process.env.JWT_KEY, {
-            expiresIn: 360000,
-        });
-        res
-            .status(201)
-            .json({ token, user: {...user._doc, password: undefined } });
-        return Promise.resolve();
-    } catch (error) {
-        console.log(error.message);
-        res.status(500).send('Server Error!');
-        return Promise.reject();
-    }
+    const token = jwt.sign({ id: user._id }, process.env.JWT_KEY, {
+      expiresIn: 360000,
+    });
+    res
+      .status(201)
+      .json({ token, user: { ...user._doc, password: undefined } });
+    return Promise.resolve();
+  } catch (error) {
+    console.log(error.message);
+    res.status(500).send('Server Error!');
+    return Promise.reject();
+  }
 };
-const logIn = async(req, res, next) => {
-    const { email, password } = req.body;
-    if (!email.trim()) return res.status(400).send('Email is required!');
-    if (!password.trim()) return res.status(400).send('Password is required!');
-    try {
-        const user = await User.findOne({ email }).lean();
-        if (!user) res.status(400).send('User does not exist!');
+const logIn = async (req, res, next) => {
+  const errors = validationResult(req);
+  const { email, password } = req.body;
+  if (!errors.isEmpty())
+    return res.status(400).json({ errors: errors.array() });
+  try {
+    const user = await User.findOne({ email }).lean();
+    if (!user) return res.status(400).send('User does not exist!');
 
-        const isMatch = await bcrypt.compare(password, user.password);
-        if (!isMatch) res.status(400).send('Invalid credentials!');
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch) return res.status(400).send('Invalid credentials!');
 
-        const token = jwt.sign({ id: user._id }, process.env.JWT_KEY, {
-            expiresIn: 360000,
-        });
-        res.status(200).json({ token, user: {...user, password: undefined } });
-        return Promise.resolve();
-    } catch (error) {
-        console.log(error.message);
-        res.status(500).send('Server Error!');
-        return Promise.reject();
-    }
+    const token = jwt.sign({ id: user._id }, process.env.JWT_KEY, {
+      expiresIn: 360000,
+    });
+    res.status(200).json({ token, user: { ...user, password: undefined } });
+    return Promise.resolve();
+  } catch (error) {
+    console.log(error.message);
+    res.status(500).send('Server Error!');
+    return Promise.reject();
+  }
 };
-const authUser = async(req, res, next) => {
-    const { id, iat, exp } = req.user;
-    try {
-        const user = await User.findById(id).lean();
-        res.status(200).json({ user: {...user, password: undefined, iat, exp } });
-        return Promise.resolve();
-    } catch (error) {
-        console.log(error.message);
-        res.status(500).send('Server Error!');
-        return Promise.reject();
-    }
+const authUser = async (req, res, next) => {
+  const { id, iat, exp } = req.user;
+  try {
+    const user = await User.findById(id).lean();
+    res.status(200).json({ user: { ...user, password: undefined, iat, exp } });
+    return Promise.resolve();
+  } catch (error) {
+    console.log(error.message);
+    res.status(500).send('Server Error!');
+    return Promise.reject();
+  }
 };
 
 module.exports = { signUp, logIn, authUser };
--- routes/auth.js
+++ routes/auth.js
@@ -1,4 +1,5 @@
 const { Router } = require('express');
+const { check } = require('express-validator');
 const isAuth = require('../middleware/is-auth');
 
 const router = new Router({ strict: true });
@@ -6,8 +7,32 @@ const router = new Router({ strict: true });
 // Controllers
 const { signUp, logIn, authUser } = require('../controllers/auth');
 
-router.post('/signup', signUp);
-router.post('/login', logIn);
+router.post(
+  '/signup',
+  [
+    check('email')
+      .isEmail()
+      .normalizeEmail()
+      .withMessage('invalid email'),
+    check('password')
+      .isLength({ min: 5 })
+      .withMessage('must be at least 5 chars long'),
+  ],
+  signUp
+);
+router.post(
+  '/login',
+  [
+    check('email')
+      .isEmail()
+      .normalizeEmail()
+      .withMessage('invalid email'),
+    check('password')
+      .isLength({ min: 5 })
+      .withMessage('must be at least 5 chars long'),
+  ],
+  logIn
+);
 router.get('/user', isAuth, authUser);
 
 module.exports = router;
